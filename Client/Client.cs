307dda93f8c059bbbb65aa33877c4061aa0b9836
Bezimienii <czerminskijakub@gmail.com>
2023-05-18 19:14:04 +0200

    Add project files.

---
+++ Client/Client.cs
@@ -0,0 +1,87 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MassTransit;
+using Messages;
+using RabbitMQ;
+
+class ClientConsumer : IConsumer<IConfirmationCheck>, IConsumer<IAcceptOrder>
+{
+    private String ClientName;
+    public ClientConsumer(String NameOfClient)
+    {
+        this.ClientName = NameOfClient;
+    }
+    public Task Consume(ConsumeContext<IConfirmationCheck> context)
+    {
+        if(context.Message.ID == this.ClientName)
+        {
+            Console.WriteLine($"Should the order with ID {context.Message.CorrelationId} be accepted?");
+            bool odp = false;
+            if(Console.ReadKey().Key == ConsoleKey.T)
+            {
+                odp = true;
+            }
+            return Task.Run(() =>
+            {
+                context.RespondAsync(new Confirmation() { CorrelationId = context.Message.CorrelationId, HasConfirmed = odp });
+            }
+            );
+        }
+        else
+        {
+            return Task.Run(() => { });
+        }
+    }
+
+    public Task Consume(ConsumeContext<IAcceptOrder> context)
+    {
+        if(context.Message.ID == this.ClientName)
+        {
+            String OrderStatus = context.Message.HasAcceptedOrder ? "zaakceptowany" : "odrzucony";
+            return Console.Out.WriteLineAsync($"Order has been {OrderStatus}");
+        }
+        else
+        {
+            return Task.Run(() => { });
+        }
+    }
+}
+
+class Client 
+{
+    static void Main(string[] args)
+    {
+        String ClientName = "Client";
+        var queue = new ClientConsumer(ClientName);
+        var bus = Bus.Factory.CreateUsingRabbitMq(
+            sbc =>
+            {
+                sbc.Host(
+                    new Uri("rabbitmq://localhost/username"),
+                    h => { h.Username("guest"); h.Password("guest"); }
+                );
+                sbc.ReceiveEndpoint(ClientName,
+                    ep => ep.Instance(queue));
+            }
+        );
+        bus.Start();
+        while(true)
+        {
+            if(Console.ReadKey().Key == ConsoleKey.B)
+            {
+                Console.WriteLine("In what quantity do you want the goods?");
+
+                int quantity = 0;
+                try
+                {
+                    quantity = Convert.ToInt32(Console.ReadLine());
+                    bus.Publish(new SendReservationRequest() { ID = ClientName, Amount = quantity });
+                    Console.WriteLine();
+                }
+                catch(Exception) 
+                {
+                    Console.WriteLine("\nThere should be a number entered")
+                }
+            }
+        }
+    }
+}
---
+++ Komunikaty/Messages.cs
@@ -0,0 +1,80 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MassTransit;
+
+namespace Messages
+{
+    public interface ISendReservationRequest
+    {    
+        string ID { get; set; }
+        int Amount { get; set; }
+    }
+    public class SendReservationRequest : ISendReservationRequest
+    {
+        public string ID { get; set; }
+        public int Amount { get; set; }
+    }
+    public interface IConfirmationCheck : CorrelatedBy<Guid> 
+    {
+        string ID { get; set; }
+    }
+    public class ConfirmationCheck : IConfirmationCheck
+    {
+        public string ID { get; set; }
+        public Guid CorrelationId { get; set; }
+    }
+    public interface IConfirmationRequest : CorrelatedBy<Guid>
+    {
+    }
+    public class ConfirmationRequest : IConfirmationRequest
+    {
+        public Guid CorrelationId { get; set; }
+    }
+    public interface IConfirmation : CorrelatedBy<Guid>
+    {
+        bool HasConfirmed { get; set; }
+    }
+
+    public class Confirmation : IConfirmation
+    {
+        public bool HasConfirmed { get; set; }
+        public Guid CorrelationId { get; set; }
+    }
+    public interface ICheckAmountRequest : CorrelatedBy<Guid>
+    {
+    }
+    public class CheckAmountRequest : ICheckAmountRequest
+    {
+        public Guid CorrelationId { get; set; }
+    }
+    public interface ICheckAmountResponse : CorrelatedBy<Guid>
+    {
+        bool IsAmountAvailable { get; set; }
+    }
+    public class CheckAmountResponse : ICheckAmountResponse
+    {
+        public bool IsAmountAvailable { get; set; }
+        public Guid CorrelationId { get; set; }
+    }
+    public interface IAcceptOrder : CorrelatedBy<Guid>
+    {
+        bool HasAcceptedOrder { get; set; }
+        string ID { get; set; };
+    }
+    public class AcceptOrder: IAcceptOrder
+    {
+        public bool HasAcceptedOrder { get; set; }
+        public string ID { get; set; }
+        public Guid CorrelationId { get; set; }
+    }
+
+    public interface ITimeout : CorrelatedBy<Guid>
+    {
+    }
+    public class Timeout : ITimeout
+    {
+        public Guid CorrelationId { get; set; }
+    }
+
+
+
+}
